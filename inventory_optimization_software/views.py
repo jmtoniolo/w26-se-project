578952de38c7e8edb1b6bd45378ca2b7e5b146b8
Vesper <john.m.toniolo@gmail.com>
2026-02-11 20:51:44 -0500

    Get splash page started with a button that does nothing. Lets git 'er done!

--- inventory_optimization_software/views.py
+++ inventory_optimization_software/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+from django.shortcuts import render
+
 def index(request):
-    return HttpResponse("Hello, world, I'm iOS. I optimize inventories!")
+    # This renders the 'base.html' template we created earlier
+    #return HttpResponse("Hello, world, I'm iOS. I optimize inventories!")
+    return render(request, 'inventorysplash.html')
+
+def add_item(request):
+    # This is a placeholder for your "Add" logic
+    return render(request, 'add_item.html')
+
+# Create your views here.
+# def index(request):
+#     return HttpResponse("Hello, world, I'm iOS. I optimize inventories!")
