f72705581baddb4f86ffb522f4e471ebb05012ee
cole-s <schaarca@gmail.com>
2026-04-16 20:52:01 -0500

    reverting changes

--- iOS/urls.py
+++ iOS/urls.py
@@ -17,10 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from inventory_optimization_software import views
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('inventory_optimization_software/', include("inventory_optimization_software.urls")),
 ]
